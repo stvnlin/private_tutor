d0618b3cb8fd5ec183108951877ede755601a344
stvnlin <lin_jd@hotmail.com>
2018-06-06 09:53:18 +0800

    update 2018-06-06

--- C++/math.cpp
+++ C++/math.cpp
@@ -24,6 +24,40 @@ void math_problem1(){
     }
 }
 
+long long fibo_1(int n){
+    if(n==1)
+        return 1;
+    if(n==2)
+        return 1;
+    return fibo_1(n - 1) + fibo_1(n - 2);
+}
+
+long long fibo_2(int n){
+    long long a = 0, b = 1;
+    for (int i = 0; i < n; i++)
+    {
+        b = a + b;
+        a = b - a;
+    }
+    return a;
+}
+
+void math_problem2(){
+    int n;
+    clock_t begin_t;
+
+    cout << "input the number of fibonacci serie you want:";
+    cin >> n;
+    cout << "the first approach" << endl;
+    begin_t = clock();
+    cout << "the number of " << n << "th is " << fibo_1(n) << endl;
+    cout << "it takes " << float(clock() - begin_t) / CLOCKS_PER_SEC << "seconds" << endl;
+    cout << "the second approach" << endl;
+    begin_t = clock();
+    cout << "the number of " << n << "th is " << fibo_2(n) << endl;
+    cout << "it takes " << float(clock() - begin_t) / CLOCKS_PER_SEC << "seconds" << endl;
+}
+
 int main(){
-    math_problem1();
+    math_problem2();
 }
---
+++ C++/readme.md
@@ -0,0 +1,8 @@
+#程序目录
+
+## hello.cpp
+hello world 程序
+
+## math.cpp
+* math_problem1: 计算三角数倒数和，分别计算10/100/1000/10000/100000个，以直观显示其极限
+* math_problem2: 计算fibonacci，递归法/直接循环法，比较效率
